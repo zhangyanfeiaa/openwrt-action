3e9dbecadc2df4a731c59fe893e32f834dc1ceab
zhangyanfeiaa <71303082+zhangyanfeiaa@users.noreply.github.com>
2021-08-23 23:58:12 +0800

    Update diy-k2.sh

--- diy-k2.sh
+++ diy-k2.sh
@@ -8,3 +8,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 应用过滤OpenAppFilter 【luci oaf app】
+# 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
